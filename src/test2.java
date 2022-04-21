649874ecd3c8bde373f54102d3dd76a5221b40fa
sathishn001 <34599852+sathishn001@users.noreply.github.com>
2022-04-21 12:36:46 +0530

    Initial commit

---
+++ src/test2.java
@@ -0,0 +1,11 @@
+public class test2 {
+    public static void main (String[] args){
+
+        for(int i; i>11, i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
---
+++ test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+
+    public static void main (String[] args){
+
+        for(int i; i>11, i++)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
